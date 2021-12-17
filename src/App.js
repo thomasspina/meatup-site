421003921a2585b62793607fdfa860f64c98b715
Thomas <thomas@thomasspina.com>
2021-12-17 13:49:37 -0500

    added a few components to play around with

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
+import Todo from "./components/Todo"
+
+
 function App() {
   return (
   <div>
     <h1>Todos</h1>
-    <div className="card">
-      <h2>TITLE</h2>
-      <div>
-        <button>Delete</button>
-      </div>
-    </div>
+    <Todo title="learn react"/>
   </div>
   );
 }
